0bd9dcc29e0e9ce0d4d5983fd08b80c5d942ad41
Peter Schoeffman <peter@blackbox.(none)>
2014-04-09 21:37:40 -0400

    remove php module install from custom config

--- configs/recipes/default.rb
+++ configs/recipes/default.rb
@@ -13,17 +13,17 @@ end
 nginx_site "site" #name of module
 
 #install the required php mysql extension
-package "php5-mysql" do
-  action :install
-end
+#package "php5-mysql" do
+#  action :install
+#end
 
 # create a mysql database - use mysql -u root -proot in order to see database
 mysql_database 'siteDb' do connection ({:host => "localhost", :username => 'root', :password => "root"})
 
   action :create
 end
-#
-#execute "import_database" do
-#  command "mysql -u root -p#{node['mysql']['server_root_password']} siteDb < #{node['mysql']['projectRoot']}#{node['mysql']['dump_filename']}"
-#  action :run
-#end
+
+execute "import_database" do
+  command "mysql -u root -p#{node['mysql']['server_root_password']} siteDb < #{node['mysql']['projectRoot']}#{node['mysql']['dump_filename']}"
+  action :run
+end
