5cd7d77a34f328c3602eea917247829cd68dbc99
Peter Schoeffman <peter@blackbox.(none)>
2014-04-13 18:12:57 -0400

    created mysql recipe and removed old nginx files

--- configs/recipes/mysql.rb
+++ configs/recipes/mysql.rb
@@ -3,11 +3,11 @@ include_recipe "mysql::server"
 include_recipe "database::mysql"
 
 # create a mysql database - use mysql -u root -proot in order to see database
-mysql_database 'siteDb' do connection ({:host => "localhost", :username => 'root', :password => "root"})
+mysql_database node['vagrantConfig']['databaseName'] do connection ({:host => "localhost", :username => 'root', :password => "root"})
   action :create
 end
 
 execute "import_database" do
-  command "mysql -u root -p#{node['mysql']['server_root_password']} siteDb < #{node['mysql']['projectRoot']}#{node['mysql']['dump_filename']}"
+  command "mysql -u root -p#{node['mysql']['server_root_password']} #{node['vagrantConfig']['databaseName']} < #{node['mysql']['projectRoot']}#{node['mysql']['dump_filename']}"
   action :run
 end
